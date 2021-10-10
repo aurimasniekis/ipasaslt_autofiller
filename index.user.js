a6243953039734798a662baafdb6ff8bc9f6d5b0
Aurimas Niekis <aurimas@niekis.lt>
2021-10-10 10:05:08 +0300

    Update index.user.js

--- index.user.js
+++ index.user.js
@@ -7,10 +7,9 @@
 // @downloadURL  https://raw.githubusercontent.com/aurimasniekis/ipasaslt_autofiller/master/index.user.js
 // @version      1.0.0
 // @license      MIT
-// @run-at       document-idle
 // @description  Automatically fills your data into website, no more typing same numbers again
 // @author       Aurimas Niekis
-// @match        https://www.ipasas.lt/*
+// @match        https://www.ipasas.lt/index.php*
 // @icon         https://www.google.com/s2/favicons?domain=ipasas.lt
 // @require      https://openuserjs.org/src/libs/sizzle/GM_config.js
 // @grant        GM_getValue
